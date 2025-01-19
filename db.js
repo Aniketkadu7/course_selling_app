0731f27f47d7bd39eef3f4db218ec80d2f7f4348
Sersei7 <kaduaniket718@gmail.com>
2025-01-19 12:56:36 +0530

    Created the schema structures in database

---
+++ db.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://Sersei7:serseiisme@cluster0.wsolr.mongodb.net/course_Selling_app");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+user  = new Schema({
+    email : {type :String, unique:true},
+    password : String,
+    username : String,
+    firstName : String,
+    lastName : String,
+    __id : ObjectId
+});
+
+admin = new Schema({
+    email : {type :String, unique:true},
+    password : String,
+    username : String,
+    firstName : String,
+    lastName : String,
+    __id : ObjectId
+
+
+})
+
+course = new Schema({
+    title : String,
+    price : Number,
+    description : String,
+    creatorid : ObjectId
+
+})
+
+purchase = new Schema({
+    __id : ObjectId,
+    userId : ObjectId,
+    coursId : ObjectId
+})
+
+const userModel = mongoose.model("user",user);
+const adminModel = mongoose.model("admin", admin);
+const courseModel = mongoose.model("course", course);
+const purchaseModel = mongoose.model("purchase",purchase);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
